c0296299fab5365f52a73cec89f11beed10cfa72
sajithnsilvame <sajithnsilva.me@gmail.com>
2024-06-08 12:07:32 +0530

    fix the mongodb timeout

--- libs/mongodb.js
+++ libs/mongodb.js
@@ -1,13 +1,16 @@
 import mongoose from "mongoose";
 
-const connectMongoDB = async () =>{
-    try{
-        await mongoose.connect(process.env.MONGODB_URI);
-        console.log("Connected to MongoDB");
-    }
-    catch(error){
-        console.log(error);
-    }
-}
+const connectMongoDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 5000, // 5 seconds timeout
+    });
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.log("Error connecting to MongoDB:", error);
+  }
+};
 
 export default connectMongoDB;
